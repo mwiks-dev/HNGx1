0004dfc6b5d6ff58559cab36e8fc8851755a0b28
Maryann Mwikali <mwikali119@gmail.com>
2023-09-10 01:35:19 +0300

    Change app route to 'api'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/get_info', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_info():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
