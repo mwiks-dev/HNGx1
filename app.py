01e40415771f2cdb2c33f13ce01005887f593f73
Maryann Mwikali <mwikali119@gmail.com>
2023-09-11 12:37:28 +0300

    Update time code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import datetime
+from datetime import datetime, timedelta, timezone
 
 app = Flask(__name__)
 
@@ -12,17 +12,24 @@ def get_info():
         return jsonify({'error': 'Both slack_name and track are required'}), 400
 
     # Get the current day of the week
-    current_day = datetime.datetime.utcnow().strftime('%A')
+    current_day = datetime.utcnow().strftime('%A')
 
-    # Get the current UTC time with validation of +/-2 minutes
-    current_time = datetime.datetime.utcnow()
-    time_with_offset = current_time + datetime.timedelta(minutes=2)
+    # Get the current datetime in UTC
+    current_datetime = datetime.now(timezone.utc)
 
+    # Calculate the offset from UTC in minutes
+    timezone_offset_minutes = current_datetime.utcoffset().total_seconds() // 60
+
+    # Create a new datetime object with the offset added
+    utc_datetime = current_datetime + timedelta(minutes=timezone_offset_minutes)
+
+    # Convert the datetime to ISO8601 format with 'Z'
+    utc_time = utc_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
     # Construct the response JSON
     response_data = {
         'slack_name': slack_name,
         'current_day': current_day,
-        'utc_time': (datetime.datetime.utcnow() + datetime.timedelta(minutes=2)).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'utc_time': utc_time,
         'track': track,
         'github_file_url': 'https://github.com/mwiks-dev/HNGx1/blob/main/app.py',  # Replace with your file URL
         'github_repo_url': 'https://github.com/mwiks-dev/HNGx1',  # Replace with your repo URL
